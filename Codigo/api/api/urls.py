ac29d3f882badfe1671b7c20d388e9432d197db6
IsaacCR <isaaccivan@gmail.com>
2015-03-13 23:55:49 -0600

    Agregando Evento

--- Codigo/api/api/urls.py
+++ Codigo/api/api/urls.py
@@ -6,7 +6,11 @@ urlpatterns = patterns('',
     # url(r'^$', 'api.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
    
-	  url(r'^Cliente/$', 'api.views.Cliente_list', name='Cliente_list'),
-    url(r'^Cliente/(?P<pk>[0-9]+)$', 'api.views.Cliente_detail', name='Cliente_detail'),
+	 url(r'^Cliente/$', 'api.views.Cliente_list', name='Cliente_list'),
+	 url(r'^Cliente/(?P<pk>[0-9]+)$', 'api.views.Cliente_detail', name='Cliente_detail'),
+    
+	url(r'^Evento/$', 'api.views.Evento_list', name='Evento_list'),
+	url(r'^Evento/(?P<pk>[0-9]+)$', 'api.views.Evento_detail', name='Evento_detail'),
+    
     url(r'^admin/', include(admin.site.urls)),
 )
--- Codigo/api/api/views.py
+++ Codigo/api/api/views.py
@@ -26,6 +26,7 @@ def Cliente_list(request):
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def Cliente_detail(request, pk):
+
     """
     Get, udpate, or delete a specific task
     """
@@ -50,4 +51,53 @@ def Cliente_detail(request, pk):
     elif request.method == 'DELETE':
         task.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-	   
+##Finn
+
+
+####inicio Evento
+@api_view(['GET', 'POST'])
+def Evento_list(request):
+    """
+    List all tasks, or create a new task.
+    """
+    if request.method == 'GET':
+        tasks = Evento.objects.all()
+        serializer = EventoSerializer(tasks, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer =postEventoSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def Evento_detail(request, pk):
+
+    """
+    Get, udpate, or delete a specific task
+    """
+    try:
+        task = Evento.objects.get(pk=pk)
+    except task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = EventoSerializer(task)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = EventoSerializer(task, data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(
+                serilizer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+##Finn
